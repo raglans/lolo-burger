434ac1489fd7472cc4c03f7222cefc9f66f8395e
nikola@nikola.nikola <nikola@nikola.nikola>
2024-08-21 10:51:44 -0700

    update image and changed styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 import Image from 'next/image';
-import Burger from '../../public/images/burger.jpeg';
+import Burger from '../../public/images/burger.jpg';
 import { Dancing_Script } from "next/font/google";
 
 const dancing_script = Dancing_Script({ subsets: ["latin"] });
@@ -9,15 +9,14 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={`${styles.center} ${styles.our_approach}`}>
-          <h3 className={`${styles.subtitle} ${dancing_script.className}`} style={{margin: '0'}}>Happy 10th Anniversary</h3>
-          <h3 className={`${styles.subtitle} ${dancing_script.className}`} style={{margin: '0'}}>to the </h3>
+          <h3 className={`${styles.subtitle} ${dancing_script.className}`} style={{margin: '0', marginTop: '5%'}}>Happy 7th Anniversary</h3>
           <h1 className={styles.title}>LoLo® Burger</h1>
         <div className={`${styles.center} ${styles.our_approach}`}>
-          <Image style={{marginTop: '15%'}} src={Burger} />
-          <p style={{margin: '15% 15%'}}>
-            Our signature burger was created in 2014 under our moniker celebrating local creativity & small business independence.
+          <Image style={{width: '50%', height: '50%'}} src={Burger} />
+          <p style={{margin: '7% 20% 7% 20%'}}>
+            Our signature burger was created in 2017 under our moniker celebrating local creativity & small business independence.
           </p>
-          <a className={styles.button} href="https://www.raglans.ca/">Raglans</a>
+          <a className={styles.button} href="https://www.raglans.ca/">Order Now</a>
         </div>
       </div>
     </main>
